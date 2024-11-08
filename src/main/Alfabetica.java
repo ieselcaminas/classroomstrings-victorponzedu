88bb5f4318329a9255199d7f671a7111efeab668
victor ponz <victorponz@gmail.com>
2024-11-08 12:53:06 +0100

    Alfabética

--- src/main/Alfabetica.java
+++ src/main/Alfabetica.java
@@ -1,6 +1,14 @@
 public class Alfabetica {
     public static boolean esAlfabetica(String palabra){
-
+        //amor
+        //Recorrer todo el array hasta la última porque esta no tiene siguiente
+        for (int i = 0; i < palabra.length() - 1; i++) {
+            //Si la letra es mayor que la siguiente es que está mal
+            if (palabra.charAt(i) > palabra.charAt(i + 1)){
+                return false;
+            }
+        }
+        //Si llega hasta aquí es porque está bien
         return true;
     }
 }
