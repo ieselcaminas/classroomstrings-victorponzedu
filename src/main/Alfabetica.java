545480ff65a1733aebe506ee9ef9639ec6c5c9b7
victor ponz <victorponz@gmail.com>
2024-11-08 12:55:38 +0100

    Corregir comentario

--- src/main/Alfabetica.java
+++ src/main/Alfabetica.java
@@ -1,7 +1,8 @@
 public class Alfabetica {
     public static boolean esAlfabetica(String palabra){
         //amor
-        //Recorrer todo el array hasta la última porque esta no tiene siguiente
+        // Recorrer todo el array hasta la penúltima porque
+        // la última no tiene siguiente
 
         for (int i = 0; i < palabra.length() - 1; i++) {
             //Si la letra es mayor que la siguiente es que está mal
